77c111490c15fbfe3d39be342aca034488e44bf1
Arpan Suravi Prasad <prasadar@iis.ee.ethz.ch>
2024-04-25 12:00:24 +0200

    fixed alignments and correct hci version

--- rtl/fir_streamer.sv
+++ rtl/fir_streamer.sv
@@ -39,11 +39,11 @@ module fir_streamer
   hwpe_stream_intf_stream.sink   y_i,
 
   // TCDM ports
-  hci_core_intf.initiator           tcdm [MP-1:0],
+  hci_core_intf.initiator        tcdm [MP-1:0],
 
   // control channel
-  input  fir_streamer_ctrl_t  ctrl_i,
-  output fir_streamer_flags_t flags_o
+  input  fir_streamer_ctrl_t     ctrl_i,
+  output fir_streamer_flags_t    flags_o
 );
 
   // Interface declarations
@@ -71,8 +71,8 @@ module fir_streamer
   // a streaming protocol and a data-flow paradigm.
   // x source
   hci_core_source #(
-    .MISALIGNED_ACCESSES ( 0         )
-  ) i_x_source (
+    .MISALIGNED_ACCESSES( 0                      )
+  ) i_x_source          (
     .clk_i              ( clk_i                  ),
     .rst_ni             ( rst_ni                 ),
     .test_mode_i        ( test_mode_i            ),
@@ -85,8 +85,8 @@ module fir_streamer
   );
   // h source
   hci_core_source #(
-    .MISALIGNED_ACCESSES ( 0         )
-  ) i_h_source (
+    .MISALIGNED_ACCESSES( 0                      )
+  ) i_h_source          (
     .clk_i              ( clk_i                  ),
     .rst_ni             ( rst_ni                 ),
     .test_mode_i        ( test_mode_i            ),
@@ -99,17 +99,17 @@ module fir_streamer
   );
   // y sink
   hci_core_sink #(
-    .MISALIGNED_ACCESSES ( 0         )
-  ) i_y_sink (
-    .clk_i       ( clk_i                  ),
-    .rst_ni      ( rst_ni                 ),
-    .test_mode_i ( test_mode_i            ),
-    .clear_i     ( clear_i                ),
-    .enable_i    ( enable_i               ),
-    .tcdm        ( tcdm_fifo[Y_STREAM_IDX]),
-    .stream      ( y_mem                  ),
-    .ctrl_i      ( ctrl_i.y_sink_ctrl     ),
-    .flags_o     ( flags_o.y_sink_flags   )
+    .MISALIGNED_ACCESSES( 0                      )
+  ) i_y_sink            (
+    .clk_i              ( clk_i                  ),
+    .rst_ni             ( rst_ni                 ),
+    .test_mode_i        ( test_mode_i            ),
+    .clear_i            ( clear_i                ),
+    .enable_i           ( enable_i               ),
+    .tcdm               ( tcdm_fifo[Y_STREAM_IDX]),
+    .stream             ( y_mem                  ),
+    .ctrl_i             ( ctrl_i.y_sink_ctrl     ),
+    .flags_o            ( flags_o.y_sink_flags   )
   );
 
   // TCDM-side FIFOs (here mainly as an example). We may need these
@@ -121,37 +121,37 @@ module fir_streamer
   // designed to maintain this desirable property on the memory side.
   // x fifo
   hci_core_fifo #(
-    .FIFO_DEPTH ( 2         )
-  ) i_x_tcdm_fifo (
-    .clk_i       ( clk_i                  ),
-    .rst_ni      ( rst_ni                 ),
-    .clear_i     ( clear_i                ),
-    .flags_o     (                        ),
-    .tcdm_target  ( tcdm_fifo[X_STREAM_IDX]),
+    .FIFO_DEPTH     ( 2                      )
+  ) i_x_tcdm_fifo   (
+    .clk_i          ( clk_i                  ),
+    .rst_ni         ( rst_ni                 ),
+    .clear_i        ( clear_i                ),
+    .flags_o        (                        ),
+    .tcdm_target    ( tcdm_fifo[X_STREAM_IDX]),
     .tcdm_initiator ( tcdm[X_STREAM_IDX]     )
   );
   // h fifo
   hci_core_fifo #(
-    .FIFO_DEPTH ( 2         )
-  ) i_h_tcdm_fifo (
-    .clk_i       ( clk_i                  ),
-    .rst_ni      ( rst_ni                 ),
-    .clear_i     ( clear_i                ),
-    .flags_o     (                        ),
-    .tcdm_target  ( tcdm_fifo[H_STREAM_IDX]),
+    .FIFO_DEPTH     ( 2                      )
+  ) i_h_tcdm_fifo   (
+    .clk_i          ( clk_i                  ),
+    .rst_ni         ( rst_ni                 ),
+    .clear_i        ( clear_i                ),
+    .flags_o        (                        ),
+    .tcdm_target    ( tcdm_fifo[H_STREAM_IDX]),
     .tcdm_initiator ( tcdm[H_STREAM_IDX]     )
   );
   // y fifo
   hci_core_fifo #(
-    .FIFO_DEPTH ( 2         )
-  ) i_y_tcdm_fifo (
-    .clk_i       ( clk_i                  ),
-    .rst_ni      ( rst_ni                 ),
-    .clear_i     ( clear_i                ),
-    .flags_o     (                        ),
-    .tcdm_target  ( tcdm_fifo[Y_STREAM_IDX]),
+    .FIFO_DEPTH     ( 2                      )
+  ) i_y_tcdm_fifo   (
+    .clk_i          ( clk_i                  ),
+    .rst_ni         ( rst_ni                 ),
+    .clear_i        ( clear_i                ),
+    .flags_o        (                        ),
+    .tcdm_target    ( tcdm_fifo[Y_STREAM_IDX]),
     .tcdm_initiator ( tcdm[Y_STREAM_IDX]     )
-  );
+  ); 
 
   // As the memory and datapath sides use different data sizes, we need
   // to split and serialize incoming streams and deserialize and merge
@@ -160,116 +160,116 @@ module fir_streamer
   hwpe_stream_split #(
     .NB_OUT_STREAMS ( MEM_WIDTH/DATA_WIDTH ),
     .DATA_WIDTH_IN  ( MEM_WIDTH            )
-  ) i_x_split (
-    .clk_i       ( clk_i   ),
-    .rst_ni      ( rst_ni  ),
-    .clear_i     ( clear_i ),
-    .push_i      ( x_mem   ),
-    .pop_o       ( x_split )
+  ) i_x_split       (
+    .clk_i          ( clk_i                ),
+    .rst_ni         ( rst_ni               ),
+    .clear_i        ( clear_i              ),
+    .push_i         ( x_mem                ),
+    .pop_o          ( x_split              )
   );
   // x split FIFO -- necessary to avoid deadlocks with serializer in SYNC_READY mode
   for(genvar ii=0; ii<MEM_WIDTH/DATA_WIDTH; ii++) begin : x_split_fifo_gen
     hwpe_stream_fifo #(
-      .FIFO_DEPTH ( 2         ),
-      .DATA_WIDTH ( DATA_WIDTH )
-    ) i_h_split_fifo (
-      .clk_i       ( clk_i                  ),
-      .rst_ni      ( rst_ni                 ),
-      .clear_i     ( clear_i                ),
-      .flags_o     (                        ),
-      .push_i      ( x_split[ii]            ),
-      .pop_o       ( x_split_postfifo[ii]   )
+      .FIFO_DEPTH   ( 2                      ),
+      .DATA_WIDTH   ( DATA_WIDTH             )
+    ) i_h_split_fifo(
+      .clk_i        ( clk_i                  ),
+      .rst_ni       ( rst_ni                 ),
+      .clear_i      ( clear_i                ),
+      .flags_o      (                        ),
+      .push_i       ( x_split[ii]            ),
+      .pop_o        ( x_split_postfifo[ii]   )
     );
   end
   // x serialize -- the serializer requires SYNC_READY mode to deal with streams
   // generated by the same producer (here: the x source)
   hwpe_stream_serialize #(
-    .NB_IN_STREAMS ( MEM_WIDTH/DATA_WIDTH ),
-    .DATA_WIDTH    ( DATA_WIDTH           ),
-    .SYNC_READY    ( 1'b1                 )
-  ) i_x_serialize (
-    .clk_i       ( clk_i                   ),
-    .rst_ni      ( rst_ni                  ),
-    .clear_i     ( clear_i                 ),
-    .ctrl_i      ( ctrl_i.x_serialize_ctrl ),
-    .push_i      ( x_split_postfifo        ),
-    .pop_o       ( x_prefifo               )
+    .NB_IN_STREAMS ( MEM_WIDTH/DATA_WIDTH    ),
+    .DATA_WIDTH    ( DATA_WIDTH              ),
+    .SYNC_READY    ( 1'b1                    )
+  ) i_x_serialize  (
+    .clk_i         ( clk_i                   ),
+    .rst_ni        ( rst_ni                  ),
+    .clear_i       ( clear_i                 ),
+    .ctrl_i        ( ctrl_i.x_serialize_ctrl ),
+    .push_i        ( x_split_postfifo        ),
+    .pop_o         ( x_prefifo               )
   );
 
   // h split
   hwpe_stream_split #(
     .NB_OUT_STREAMS ( MEM_WIDTH/DATA_WIDTH ),
     .DATA_WIDTH_IN  ( MEM_WIDTH            )
-  ) i_h_split (
-    .clk_i       ( clk_i   ),
-    .rst_ni      ( rst_ni  ),
-    .clear_i     ( clear_i ),
-    .push_i      ( h_mem   ),
-    .pop_o       ( h_split )
+  ) i_h_split       (
+    .clk_i          ( clk_i                ),
+    .rst_ni         ( rst_ni               ),
+    .clear_i        ( clear_i              ),
+    .push_i         ( h_mem                ),
+    .pop_o          ( h_split              )
   );
   // h split FIFO -- necessary to avoid deadlocks with serializer in SYNC_READY mode
   for(genvar ii=0; ii<MEM_WIDTH/DATA_WIDTH; ii++) begin : h_split_fifo_gen
     hwpe_stream_fifo #(
-      .FIFO_DEPTH ( 2         ),
-      .DATA_WIDTH ( DATA_WIDTH )
+      .FIFO_DEPTH    ( 2                   ),
+      .DATA_WIDTH    ( DATA_WIDTH          )
     ) i_h_split_fifo (
-      .clk_i       ( clk_i                  ),
-      .rst_ni      ( rst_ni                 ),
-      .clear_i     ( clear_i                ),
-      .flags_o     (                        ),
-      .push_i      ( h_split[ii]            ),
-      .pop_o       ( h_split_postfifo[ii]   )
+      .clk_i         ( clk_i               ),
+      .rst_ni        ( rst_ni              ),
+      .clear_i       ( clear_i             ),
+      .flags_o       (                     ),
+      .push_i        ( h_split[ii]         ),
+      .pop_o         ( h_split_postfifo[ii])
     );
   end
   // h serialize -- the serializer requires SYNC_READY mode to deal with streams
   // generated by the same producer (here: the x source)
   hwpe_stream_serialize #(
-    .NB_IN_STREAMS ( MEM_WIDTH/DATA_WIDTH ),
-    .DATA_WIDTH    ( DATA_WIDTH           ),
-    .SYNC_READY    ( 1'b1                 )
-  ) i_h_serialize (
-    .clk_i       ( clk_i                   ),
-    .rst_ni      ( rst_ni                  ),
-    .clear_i     ( clear_i                 ),
-    .ctrl_i      ( ctrl_i.h_serialize_ctrl ),
-    .push_i      ( h_split_postfifo        ),
-    .pop_o       ( h_prefifo               )
+    .NB_IN_STREAMS ( MEM_WIDTH/DATA_WIDTH    ),
+    .DATA_WIDTH    ( DATA_WIDTH              ),
+    .SYNC_READY    ( 1'b1                    )
+  ) i_h_serialize  (
+    .clk_i         ( clk_i                   ),
+    .rst_ni        ( rst_ni                  ),
+    .clear_i       ( clear_i                 ),
+    .ctrl_i        ( ctrl_i.h_serialize_ctrl ),
+    .push_i        ( h_split_postfifo        ),
+    .pop_o         ( h_prefifo               )
   );
 
   // y deserialize
   hwpe_stream_deserialize #(
-    .NB_OUT_STREAMS ( MEM_WIDTH/DATA_WIDTH ),
-    .DATA_WIDTH     ( DATA_WIDTH           )
+    .NB_OUT_STREAMS ( MEM_WIDTH/DATA_WIDTH      ),
+    .DATA_WIDTH     ( DATA_WIDTH                )
   ) i_y_deserialize (
-    .clk_i       ( clk_i                     ),
-    .rst_ni      ( rst_ni                    ),
-    .clear_i     ( clear_i                   ),
-    .ctrl_i      ( ctrl_i.y_deserialize_ctrl ),
-    .push_i      ( y_postfifo                ),
-    .pop_o       ( y_split_prefence          )
+    .clk_i          ( clk_i                     ),
+    .rst_ni         ( rst_ni                    ),
+    .clear_i        ( clear_i                   ),
+    .ctrl_i         ( ctrl_i.y_deserialize_ctrl ),
+    .push_i         ( y_postfifo                ),
+    .pop_o          ( y_split_prefence          )
   );
   // y fence
   hwpe_stream_fence #(
-    .NB_STREAMS ( MEM_WIDTH/DATA_WIDTH ),
-    .DATA_WIDTH ( DATA_WIDTH           )
-  ) i_y_fence (
-    .clk_i       ( clk_i                     ),
-    .rst_ni      ( rst_ni                    ),
-    .clear_i     ( clear_i                   ),
-    .test_mode_i ( 1'b0                      ),
-    .push_i      ( y_split_prefence          ),
-    .pop_o       ( y_split                   )
+    .NB_STREAMS  ( MEM_WIDTH/DATA_WIDTH   ),
+    .DATA_WIDTH  ( DATA_WIDTH             )
+  ) i_y_fence    (
+    .clk_i       ( clk_i                  ),
+    .rst_ni      ( rst_ni                 ),
+    .clear_i     ( clear_i                ),
+    .test_mode_i ( 1'b0                   ),
+    .push_i      ( y_split_prefence       ),
+    .pop_o       ( y_split                )
   ); 
   // y merge
   hwpe_stream_merge #(
     .NB_IN_STREAMS ( MEM_WIDTH/DATA_WIDTH ),
     .DATA_WIDTH_IN ( DATA_WIDTH           )
-  ) i_y_merge (
-    .clk_i       ( clk_i   ),
-    .rst_ni      ( rst_ni  ),
-    .clear_i     ( clear_i ),
-    .push_i      ( y_split ),
-    .pop_o       ( y_mem   )
+  ) i_y_merge      (
+    .clk_i         ( clk_i                ),
+    .rst_ni        ( rst_ni               ),
+    .clear_i       ( clear_i              ),
+    .push_i        ( y_split              ),
+    .pop_o         ( y_mem                )
   );
 
   // Datapath-side FIFOs. These are used to decouple the datapath from
@@ -282,39 +282,39 @@ module fir_streamer
     .DATA_WIDTH( DATA_WIDTH ),
     .FIFO_DEPTH( 2          ),
     .LATCH_FIFO( 0          )
-  ) i_x_fifo (
-    .clk_i   ( clk_i     ),
-    .rst_ni  ( rst_ni    ),
-    .clear_i ( clear_i   ),
-    .push_i  ( x_prefifo ),
-    .pop_o   ( x_o       ),
-    .flags_o (           )
+  ) i_x_fifo   (
+    .clk_i     ( clk_i      ),
+    .rst_ni    ( rst_ni     ),
+    .clear_i   ( clear_i    ),
+    .push_i    ( x_prefifo  ),
+    .pop_o     ( x_o        ),
+    .flags_o   (            )
   );
 
   hwpe_stream_fifo #(
     .DATA_WIDTH( DATA_WIDTH ),
     .FIFO_DEPTH( 2          ),
     .LATCH_FIFO( 0          )
-  ) i_h_fifo (
-    .clk_i   ( clk_i     ),
-    .rst_ni  ( rst_ni    ),
-    .clear_i ( clear_i   ),
-    .push_i  ( h_prefifo ),
-    .pop_o   ( h_o       ),
-    .flags_o (           )
+  ) i_h_fifo   (
+    .clk_i     ( clk_i      ),
+    .rst_ni    ( rst_ni     ),
+    .clear_i   ( clear_i    ),
+    .push_i    ( h_prefifo  ),
+    .pop_o     ( h_o        ),
+    .flags_o   (            )
   );
 
   hwpe_stream_fifo #(
     .DATA_WIDTH( DATA_WIDTH ),
     .FIFO_DEPTH( 2          ),
     .LATCH_FIFO( 0          )
-  ) i_y_fifo (
-    .clk_i   ( clk_i      ),
-    .rst_ni  ( rst_ni     ),
-    .clear_i ( clear_i    ),
-    .push_i  ( y_i        ),
-    .pop_o   ( y_postfifo ),
-    .flags_o (            )
+  ) i_y_fifo   (
+    .clk_i     ( clk_i      ),
+    .rst_ni    ( rst_ni     ),
+    .clear_i   ( clear_i    ),
+    .push_i    ( y_i        ),
+    .pop_o     ( y_postfifo ),
+    .flags_o   (            )
   );
 
 endmodule // fir_streamer
